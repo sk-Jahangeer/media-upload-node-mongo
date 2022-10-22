25e5e67e1d3cefa001b546674cd3e592b42c4006
scshiv29-dev <shivchaudhary790@hotmail.com>
2022-10-22 10:01:49 +0530

    Update upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.post("/upload", upload.single("file"), async (req, res) => {
     if (req.file === undefined) return res.send("you must select a file.");
-    const imgUrl = `http://localhost:8080/file/${req.file.filename}`;
-    return res.send(imgUrl);
+    const imgUrl = `${req.protocol}://${req.get("host")}/file/${req.file.filename}`;
+    return res.json(imgUrl);
 });
 
 module.exports = router;
